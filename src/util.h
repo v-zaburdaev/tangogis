56b64ee605a6279657220dd174181d56a8621873
v.zaburdaev <v.zaburdaev@gmail.com>
2012-02-20 18:15:06 +0000

    вынес file_type_test в util.c -  проверка расширения нужна не только в geo_photos.

--- src/util.h
+++ src/util.h
@@ -1,3 +1,8 @@
+#include <glib.h>
+
+gboolean
+file_type_test(const char *file, char *type);
 
 int
-sql_execute(char *db_name, char *sql, int (*cb_func)(void*,int,char**,char**));
+	sql_execute(char *db_name, char *sql, int (*cb_func)(void*,int,char**,char**));
+
